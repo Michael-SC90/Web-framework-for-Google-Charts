6d658905fcda26c4b195a7edd0b613862848aa6d
Michael Craig <34893491+Michael-SC90@users.noreply.github.com>
2020-04-29 09:44:58 -0700

    Update clear.php
    Added description to script.

--- scripts/clear.php
+++ scripts/clear.php
@@ -1,4 +1,9 @@
 <?php
+    /*
+        Deletes hashed records storing compiled chart data from database; forces app to reaggregate data from view.
+    */
+
+    
     include_once __DIR__."/connect.php";
     $clear_json = "EXEC usp_clear_json_storage;";
     sqlsrv_query($conn, $clear_json);
